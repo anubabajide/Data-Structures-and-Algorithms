9bc35fc63b6aaf880f2e169227ee3b8bb3a399fa
anubabajide <46856621+anubabajide@users.noreply.github.com>
2020-01-13 15:05:08 +0100

    Update Lonely Number.py

--- Lonely Number.py
+++ Lonely Number.py
@@ -1,11 +1,18 @@
 def lonely_number(numbers):
-  yes=set(numbers)
-  for i in numbers:
-    numbers2=numbers
-    numbers2.remove(i)
-    no=set(numbers)
-    if bool(yes.difference(no)) is True:
-    	maybe=list(yes.difference(no))
-    	print(maybe)
-    	return maybe[0]
-  return [] 
+	#change list to set
+	yes=set(numbers)
+	# loop through all items in list
+	for _ in range(len(numbers)):
+		#remove first item 
+		x=numbers.pop(0)
+		#create a set without the removed item 
+		no=set(numbers)
+		#add removed item back to list
+		numbers.append(x)
+		
+		#If there is a difference between the two sets, there is a lonely number
+		if bool(yes.difference(no)) is True:
+			return[list(yes.difference(no))[0]]
+		
+	#return null if there is no difference
+	return []
