2282b1e95af4eee9d10f5bab9829fa8a14c557f7
anubabajide <46856621+anubabajide@users.noreply.github.com>
2020-01-13 15:13:00 +0100

    Update Moving Average from Data Stream.py
    # Your MovingAverage object will be instantiated and called as such:
    # obj = MovingAverage(size)
    # param = obj.next(val)

--- Moving Average from Data Stream.py
+++ Moving Average from Data Stream.py
@@ -1,30 +1,27 @@
 class MovingAverage:
-    """
-    @param: size: An integer
-    """
+
     def __init__(self, size):
         self.data=[None]*size
         self.front=0
         self.back=-1
         self.s=0
         self.sizer=size
-    """
-    @param: val: An integer
-    @return:  
-    """
     
-    def next(self, val):
-        
+    def next(self, val):  
+        #check if queue is full, if it is, subtract the oldest item from the sum and remove it from the queue
         if self.isFull():
             self.s -= self.data[self.front]
             self.data[self.front]=None
             self.front= (self.front+1) % self.sizer
-            
+        
+        # add the item into the queue and add it to the sum    
         self.back= (self.back+1) % self.sizer 
         self.data[self.back] = val
         self.s += val
+        
         if self.isFull():
             return (self.s/self.sizer)
+        
         return (self.s/(self.back+1))
         
     def isEmpty(self) -> bool:
@@ -42,9 +39,3 @@ class MovingAverage:
             return True
         else: 
             return False 
-        
-
-
-# Your MovingAverage object will be instantiated and called as such:
-# obj = MovingAverage(size)
-# param = obj.next(val)
