5f615e650771d16382d316f90dc5d64a2b135887
anubabajide <46856621+anubabajide@users.noreply.github.com>
2020-01-13 15:58:29 +0100

    Update Validate Palindrome.py

--- Validate Palindrome.py
+++ Validate Palindrome.py
@@ -1,18 +1,13 @@
 def is_palindrome(s):
-  l = list(s.lower())
-  x = list(s.lower())
-  for i in l: 
-    if i == ' ':
-      l.remove(' ')
-      x.remove(' ')
-  for i in range((len(l)/2)):
-    temp = l[i]
-    l[i] = l[-(i+1)]
-    l[-(i+1)] = temp
-
-  print (x)
-  print (l)
-  if x == l:
-    return True
-  else:
-    return False
+	#Remove whitespaces, convert to lower case, and store in two lists
+	l = x = list("".join(s.split()).lower())
+	#reverse one of the lists
+	for i in range((len(l)/2)):
+		temp = l[i]
+		l[i] = l[-(i+1)]
+		l[-(i+1)] = temp
+	#check if two lists are identical
+	if x == l:
+		return True
+	else:
+		return False
