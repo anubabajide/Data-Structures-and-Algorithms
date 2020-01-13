b582bf38ce7f13e45bcd3e740a8e303c97d52d3c
anubabajide <46856621+anubabajide@users.noreply.github.com>
2020-01-13 14:06:14 +0100

    Update and rename Binary Tree Preorder Traversal.py to Binary Tree Preorder Traversal (recursive).py

--- Binary Tree Preorder Traversal (recursive).py
+++ Binary Tree Preorder Traversal (recursive).py
@@ -1,19 +1,19 @@
 #Solution function, takes in a Binary Tree Node and returns a List of Items in the Tree traversed pre-order 
-def preorderTraversal(self, root: TreeNode) -> List[int]:
+def preorderTraversal(root: TreeNode) -> List[int]:
 	#array to store the items
 	array=[]
 	#recursive call on the
-	return self.dummy(root,array)
+	return dummy(root,array)
 
-def dummy(self, root, array):
+def dummy(root, array):
 	#check if there is an item in the node
 	if root is not None:
 		#Parent Node first
 		array.append(root.val)
 		#Left node second
-		self.dummy(root.left, array)
+		dummy(root.left, array)
 		#Right node last
-		self.dummy(root.right, array)
+		dummy(root.right, array)
 	return array
 
 # Definition for a binary tree node.
