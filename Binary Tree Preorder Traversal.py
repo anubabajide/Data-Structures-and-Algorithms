a8c2cf2c5c8e8a60bbf9280e6fe563312ddcaa83
anubabajide <46856621+anubabajide@users.noreply.github.com>
2020-01-13 13:17:54 +0100

    Update Binary Tree Preorder Traversal.py

--- Binary Tree Preorder Traversal.py
+++ Binary Tree Preorder Traversal.py
@@ -1,32 +1,24 @@
+#Solution function, takes in a Binary Tree Node and returns a List of Items in the Tree traversed pre-order 
+def preorderTraversal(self, root: TreeNode) -> List[int]:
+	#array to store the items
+	array=[]
+	#recursive call on the
+	return self.dummy(root,array)
+
+def dummy(self, root, array):
+	#check if there is an item in the node
+	if root is not None:
+		#Parent Node first
+		array.append(root.val)
+		#Left node second
+		self.dummy(root.left, array)
+		#Right node last
+		self.dummy(root.right, array)
+	return array
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
-class Solution:
-#Recursive Solution
-    def dummy(self, root, array):
-        if root is not None:
-            array.append(root.val)
-            self.dummy(root.left, array)
-            self.dummy(root.right, array)
-        return array
-    
-    def preorderTraversal(self, root: TreeNode) -> List[int]:
-        array=[]
-        return self.dummy(root,array)
-
-#Iterative Solution
-	# def preorderTraversal(self, root: TreeNode) -> 'List[int]':
-		# rst = []
-		# stack = []
-		# while root or stack:
-			# if root:
-				# rst.append(root.val)
-				# stack.append(root)
-				# root = root.left
-			# else:
-				# root = stack.pop().right
-		# return rst
