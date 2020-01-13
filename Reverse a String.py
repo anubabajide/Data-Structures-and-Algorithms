70ae540eba182049f3507ae80871ac03457138d7
anubabajide <46856621+anubabajide@users.noreply.github.com>
2020-01-13 15:41:05 +0100

    Update Reverse a String.py

--- Reverse a String.py
+++ Reverse a String.py
@@ -1,10 +1,13 @@
 def reverse_string(str1):
-  l = list(str1)
-  seperator = ''
-  for i in range((len(l)/2)):
-    temp = l[i]
-    l[i] = l[-(i+1)]
-    l[-(i+1)] = temp
-  str1 = seperator.join(l)
-  print (str1)
-  return str1
+	#string is immutable, change to list
+	l = list(str1)
+	
+	#in the list, switch first and last items, second and second last, third and third last...
+	for i in range((len(l)/2)):
+		temp = l[i]
+		l[i] = l[-(i+1)]
+		l[-(i+1)] = temp
+	
+	#convert list to string format
+	str1 = ''.join(l)
+	return str1
